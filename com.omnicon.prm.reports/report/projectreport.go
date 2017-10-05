99b3241a5dd9ca783cc82785e2e3296a20e44876
juantorresb <juan.torresb@hotmail.com>
2017-10-05 11:22:58 -0500

    look and feel report

--- com.omnicon.prm.reports/report/projectreport.go
+++ com.omnicon.prm.reports/report/projectreport.go
@@ -1,6 +1,7 @@
 package report
 
 import (
+	"fmt"
 	"strconv"
 
 	"encoding/json"
@@ -17,36 +18,43 @@ func ProjectAssign() string {
 	pdf := gofpdf.New("P", "mm", "letter", "")
 
 	resourceToProjects := getProjectAssign()
-	w := []float64{80, 30, 30, 20}
+	template := buildTemplate("Project Assign", pdf)
+
+	wTable := []float64{90, 30, 30, 20}
+	wSum := 0.0
+	for _, v := range wTable {
+		wSum += v
+	}
 
 	projectTable := func() {
 
 		pdf.AddPage()
+		pdf.UseTemplate(template)
+		pdf.SetY(pdf.GetY() + 30)
 
 		for nameProject, project := range resourceToProjects {
 			fill := false
 
-			pdf.Write(10, "\n")
-			pdf.Text(100, pdf.GetY(), nameProject)
-			pdf.Write(10, "\n")
-
-			createHeader(pdf)
+			createHeader(pdf, "Project Name: "+nameProject, wTable)
 
 			// Color and font restoration
-			pdf.SetFillColor(224, 235, 255)
+
+			pdf.SetFillColor(222, 225, 229)
 			pdf.SetTextColor(0, 0, 0)
 			pdf.SetFont("", "", 0)
 			for _, resources := range project {
 				//y := 20 * (j + 1)
-				pdf.CellFormat(w[0], 6, resources.ResourceName, "LR", 0, "", fill, 0, "")
-				pdf.CellFormat(w[1], 6, util.GetFechaConFormato(resources.StartDate.Unix(), util.DATEFORMAT), "LR", 0, "", fill, 0, "")
-				pdf.CellFormat(w[2], 6, util.GetFechaConFormato(resources.EndDate.Unix(), util.DATEFORMAT), "LR", 0, "", fill, 0, "")
-				pdf.CellFormat(w[3], 6, strconv.FormatFloat(resources.Hours, 'f', 1, 64), "LR", 0, "", fill, 0, "")
+				pdf.SetX(15)
+				pdf.CellFormat(wTable[0], 6, resources.ResourceName, "LR", 0, "", fill, 0, "")
+				pdf.CellFormat(wTable[1], 6, util.GetFechaConFormato(resources.StartDate.Unix(), util.DATEFORMAT), "LR", 0, "C", fill, 0, "")
+				pdf.CellFormat(wTable[2], 6, util.GetFechaConFormato(resources.EndDate.Unix(), util.DATEFORMAT), "LR", 0, "C", fill, 0, "")
+				pdf.CellFormat(wTable[3], 6, strconv.FormatFloat(resources.Hours, 'f', 1, 64), "LR", 0, "C", fill, 0, "")
 				pdf.Ln(-1)
 				fill = !fill
 			}
+			pdf.SetX(15)
+			pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
 		}
-		//pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
 	}
 
 	pdf.SetFont("Arial", "", 14)
@@ -66,37 +74,44 @@ func ResourceAssign() string {
 	pdf := gofpdf.New("P", "mm", "letter", "")
 
 	resourceToProjects := getResourceAsssign()
-	w := []float64{80, 30, 30, 20}
+	template := buildTemplate("Resource Assign", pdf)
+	fmt.Println(pdf.GetY())
+
+	wTable := []float64{90, 35, 35, 25}
+	wSum := 0.0
+	for _, v := range wTable {
+		wSum += v
+	}
 
 	projectTable := func() {
 
 		pdf.AddPage()
+		pdf.UseTemplate(template)
+		pdf.SetY(pdf.GetY() + 30)
+		fmt.Println(pdf.GetY())
 
 		for resourceName, project := range resourceToProjects {
 			fill := false
 
-			pdf.Write(10, "\n")
-			pdf.Text(100, pdf.GetY(), resourceName)
-			pdf.Write(10, "\n")
-
-			createHeader(pdf)
+			createHeader(pdf, "Resource Name: "+resourceName, wTable)
 
 			// Color and font restoration
-			pdf.SetFillColor(224, 235, 255)
+
+			pdf.SetFillColor(222, 225, 229)
 			pdf.SetTextColor(0, 0, 0)
 			pdf.SetFont("", "", 0)
 			for _, resources := range project {
-				//y := 20 * (j + 1)
-				pdf.CellFormat(w[0], 6, resources.ProjectName, "LR", 0, "", fill, 0, "")
-				pdf.CellFormat(w[1], 6, util.GetFechaConFormato(resources.StartDate.Unix(), util.DATEFORMAT), "LR", 0, "", fill, 0, "")
-				pdf.CellFormat(w[2], 6, util.GetFechaConFormato(resources.EndDate.Unix(), util.DATEFORMAT), "LR", 0, "", fill, 0, "")
-				pdf.CellFormat(w[3], 6, strconv.FormatFloat(resources.Hours, 'f', 1, 64), "LR", 0, "", fill, 0, "")
+				pdf.SetX(15)
+				pdf.CellFormat(wTable[0], 8, resources.ProjectName, "LR", 0, "", fill, 0, "")
+				pdf.CellFormat(wTable[1], 8, util.GetFechaConFormato(resources.StartDate.Unix(), util.DATEFORMAT), "LR", 0, "C", fill, 0, "")
+				pdf.CellFormat(wTable[2], 8, util.GetFechaConFormato(resources.EndDate.Unix(), util.DATEFORMAT), "LR", 0, "C", fill, 0, "")
+				pdf.CellFormat(wTable[3], 8, strconv.FormatFloat(resources.Hours, 'f', 1, 64), "LR", 0, "C", fill, 0, "")
 				pdf.Ln(-1)
 				fill = !fill
 			}
-
+			pdf.SetX(15)
+			pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
 		}
-		//pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
 	}
 
 	pdf.SetFont("Arial", "", 14)
@@ -108,7 +123,12 @@ func ResourceAssign() string {
 	return filePDF
 }
 
-func createHeader(pdf *gofpdf.Fpdf) {
+func createHeader(pdf *gofpdf.Fpdf, pName string, wTable []float64) {
+
+	pdf.Write(15, "\n")
+	pdf.Text(10, pdf.GetY(), pName)
+	pdf.Write(5, "\n")
+	pdf.SetX(15)
 	header := []string{"Name", "Start Date", "End Date", "Hours"}
 	// Colors, line width and bold font
 	pdf.SetFillColor(47, 67, 80) // 0, 92, 138
@@ -116,15 +136,25 @@ func createHeader(pdf *gofpdf.Fpdf) {
 	pdf.SetDrawColor(0, 0, 0)
 	pdf.SetLineWidth(.3)
 	pdf.SetFont("", "B", 0)
-	w := []float64{80, 30, 30, 20}
+
 	wSum := 0.0
-	for _, v := range w {
+	for _, v := range wTable {
 		wSum += v
 	}
 	for j, str := range header {
-		pdf.CellFormat(w[j], 6, str, "LR", 0, "", true, 0, "")
+		pdf.CellFormat(wTable[j], 6, str, "LR", 0, "C", true, 0, "")
 	}
 	pdf.Ln(-1)
+}
+
+func buildTemplate(pName string, pdf *gofpdf.Fpdf) gofpdf.Template {
+	template := pdf.CreateTemplate(func(tpl *gofpdf.Tpl) {
+		tpl.Image("static/img/prm.png", 10, 6, 0, 0, true, "", 0, "")
+		tpl.SetFont("Arial", "B", 16)
+		tpl.Text(80, 20, pName)
+		tpl.Ln(-1)
+	})
+	return template
 }
 
 func getAllProjects() []*domain.Project {
