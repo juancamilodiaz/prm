5cc3b95458a29d89cde369bbb61d910da31d0231
Juan Torres <juan.torresb@hotmail.com>
2018-02-19 13:44:17 -0500

    jct

---
+++ com.omnicon.prm.oauth2_proxy/providers/providers.go
@@ -0,0 +1,38 @@
+package providers
+
+import (
+	"prm/com.omnicon.prm.oauth2_proxy/cookie"
+)
+
+type Provider interface {
+	Data() *ProviderData
+	GetEmailAddress(*SessionState) (string, error)
+	GetUserName(*SessionState) (string, error)
+	Redeem(string, string) (*SessionState, error)
+	ValidateGroup(string) bool
+	ValidateSessionState(*SessionState) bool
+	GetLoginURL(redirectURI, finalRedirect string) string
+	GetLogoutURL(redirectURI string) string
+	RefreshSessionIfNeeded(*SessionState) (bool, error)
+	SessionFromCookie(string, *cookie.Cipher) (*SessionState, error)
+	CookieForSession(*SessionState, *cookie.Cipher) (string, error)
+}
+
+func New(provider string, p *ProviderData) Provider {
+	switch provider {
+	case "linkedin":
+		return NewLinkedInProvider(p)
+	case "facebook":
+		return NewFacebookProvider(p)
+	case "github":
+		return NewGitHubProvider(p)
+	case "azure":
+		return NewAzureProvider(p)
+	case "gitlab":
+		return NewGitLabProvider(p)
+	case "oidc":
+		return NewOIDCProvider(p)
+	default:
+		return NewGoogleProvider(p)
+	}
+}
